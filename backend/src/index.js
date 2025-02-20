b1254c9e60d4f506b3ceb72df4cec588634e8e14
alphacrash <sudhansu.ace@gmail.com>
2025-02-20 21:26:28 +0530

    Updated

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,9 @@
-import express from "express";
+import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
+import express from "express";
 
-import authRoutes from "./routes/auth.route.js";
 import { connectDB } from "./lib/db.js";
+import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
 const PORT = process.env.PORT;
@@ -10,6 +11,7 @@ const PORT = process.env.PORT;
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
