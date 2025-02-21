2f121c7da54243d1755dd683c928f975f715ca82
alphacrash <sudhansu.ace@gmail.com>
2025-02-21 21:21:53 +0530

    Updated backend - message

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,7 @@ import express from "express";
 
 import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 
 dotenv.config();
 const PORT = process.env.PORT;
@@ -12,7 +13,9 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 app.listen(PORT, () => {
   console.log("Server is running on PORT: " + PORT);
